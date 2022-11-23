966cc626fd7a823cef04aeba71a976bf752e1d63
Dirk Rudolph <drudolph@adobe.com>
2022-11-23 07:25:00 +0100

    feat: leader profiles flyout

--- blocks/leader-profiles/leader-profiles.js
+++ blocks/leader-profiles/leader-profiles.js
@@ -1,5 +1,6 @@
-import { createOptimizedPicture, decorateIcons } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
 
+let focusedElement;
 const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
 function getImageWidth() {
@@ -10,6 +11,51 @@ function getImageWidth() {
     return 380; // 2 columns
   }
   return 280;
+}
+
+async function handleEvent(block, closeIcon, event) {
+  const { target, type, keyCode } = event;
+  let flyoutContent = document.querySelector('.leader-profiles-flyout > div');
+  let flyout = flyoutContent && flyoutContent.parentElement;
+  let closeButton = flyoutContent && flyoutContent.querySelector('[aria-label="Close"]');
+  if (((type === 'keydown' && keyCode === 13) || type === 'click') && block.contains(target)) {
+    focusedElement = document.activeElement;
+    const li = target.closest('li');
+    if (!flyout) {
+      flyout = document.createElement('div');
+      flyout.classList.add('leader-profiles-flyout');
+      flyoutContent = document.createElement('div');
+      flyoutContent.innerHTML = li.innerHTML;
+
+      closeButton = document.createElement('span');
+      closeButton.classList.add('icon', 'icon-close', 'icon-decorated');
+      closeButton.ariaLabel = 'Close';
+      closeButton.tabIndex = 0;
+      closeButton.role = 'button';
+      closeButton.innerHTML = closeIcon;
+
+      flyoutContent.appendChild(closeButton);
+      flyout.appendChild(flyoutContent);
+      document.body.appendChild(flyout);
+    } else {
+      flyoutContent.innerHTML = li.innerHTML;
+    }
+    closeButton.focus();
+    window.requestAnimationFrame(() => flyout.classList.add('opened'));
+  }
+  if (((type === 'keydown' && keyCode === 27)
+    || (((type === 'keydown' && keyCode === 13) || type === 'click') && closeButton && closeButton.contains(target))
+    || (type === 'click' && target === flyout))
+    && flyoutContent) {
+    window.requestAnimationFrame(() => {
+      flyout.classList.remove('opened');
+      window.setTimeout(() => flyout.remove(), 200);
+      if (focusedElement) {
+        focusedElement.focus();
+      }
+      focusedElement = null;
+    });
+  }
 }
 
 export default function decorate(block) {
@@ -24,8 +70,15 @@ export default function decorate(block) {
       if (heading) {
         div.className = 'leader-profile-heading';
         heading.insertAdjacentHTML('beforeend', '<span class="icon icon-chevron-right"></span>');
-      } else if (div.children.length === 1 && div.querySelector('picture')) div.className = 'leader-profile-image';
-      else div.className = 'leader-profile-body';
+      } else if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'leader-profile-image';
+      } else {
+        div.className = 'leader-profile-body';
+        div.querySelectorAll('.icon').forEach((icon) => {
+          icon.closest('a').target = '_blank';
+          icon.closest('ul').classList.add('leader-profile-social-icons');
+        });
+      }
     });
     ul.append(li);
   });
@@ -40,4 +93,15 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(ul);
   decorateIcons(ul);
+
+  // defer adding the event listeners until flyout style and close icon are loaded
+  Promise.all([
+    fetch(`${window.hlx.codeBasePath}/icons/close.svg`).then((resp) => (resp.ok ? resp.text() : 'X')),
+    new Promise((resolve) => {
+      window.setTimeout(() => loadCSS(`${window.hlx.codeBasePath}/blocks/leader-profiles/leader-profiles-flyout.css`, resolve), 1000);
+    }),
+  ]).then(([closeIcon]) => {
+    document.addEventListener('click', handleEvent.bind(document, block, closeIcon));
+    document.addEventListener('keydown', handleEvent.bind(document, block, closeIcon));
+  });
 }

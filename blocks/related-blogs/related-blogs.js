af2b9a659986c0bbea6a178ab57efb56deb7dc3f
Hannes Hertach <hanneshertach@gmail.com>
2022-11-24 17:50:10 +0100

    implement basic card structure

--- blocks/related-blogs/related-blogs.js
+++ blocks/related-blogs/related-blogs.js
@@ -21,31 +21,13 @@ async function enrichProfiles(rArticles) {
   return rArticles;
 }
 
-async function getRelatedArticles(tag) {
-  const blogList = document.createElement('ul');
-  blogList.classList.add('related-list');
-  const response = await fetch('/insights/query-index.json');
-  const json = await response.json();
-  const qresult = json.data.filter((obj) => obj.tags.includes(tag)).slice(0, maxAutoItems);
-
-  const enrichedArticles = await enrichProfiles(qresult);
-
-  enrichedArticles.forEach((value) => {
-    const blogListItem = document.createElement('li');
-    blogListItem.classList.add('related-list-item');
-    const ttr = document.createElement('div');
-    ttr.classList.add('teasertopicrelated');
-    blogListItem.append(ttr);
-    const article = document.createElement('article');
-    article.classList.add('teaser-base');
-
-    article.innerHTML = `<div class="teaser">
-
-    <a href="${value.path}" target="_self" class="teaser-link">
+function buildCard(path, title, authorProfile = {}) {
+  return `<div class="teaser">
+    <a href="${path}" target="_self" class="teaser-link">
         <div class="teaser-container">
             <div>
                 <h2 class="teaser-description">
-                    ${value.title}
+                    ${title}
                 </h2>
             </div>
         </div>
@@ -55,43 +37,76 @@ async function getRelatedArticles(tag) {
                 <div class="authorprofile-image">
                     <div class="nc-image-base">
                         <div class="nc-image-container " itemscope="" itemtype="http://schema.org/ImageObject">
-                            <img class="nc-image" src="${value.profiles.Image ?? defaultAuthorImage}" itemprop="contentUrl" alt="" sizes="10vw">
+                            <img class="nc-image" src="${authorProfile.Image ?? defaultAuthorImage}" itemprop="contentUrl" alt="" sizes="10vw">
                         </div>
                     </div>
                 </div>
                 <div class="authorprofile-info">
-                    <div class="authorprofile-name">${value.profiles.Name ?? defaultAuthorName}</div>
-                    <div class="authorprofile-position">${value.profiles.Title ?? defaultAuthorTitle}</div>
+                    <div class="authorprofile-name">${authorProfile.Name ?? defaultAuthorName}</div>
+                    <div class="authorprofile-position">${authorProfile.Title ?? defaultAuthorTitle}</div>
                 </div>
             </div>
-    </div>
-</div>`;
+      </div>
+  </div>`;
+}
+
+function buildHeadline(parent, tagConf) {
+  const head4 = document.createElement('h4');
+  const text4head = document.createTextNode(`More ${tagConf}`);
+  head4.appendChild(text4head);
+  parent.appendChild(head4);
+}
+
+async function getRelatedArticles(filter = () => true, maxItems = maxAutoItems) {
+  const response = await fetch('/insights/query-index.json');
+  const json = await response.json();
+  const queryResult = json.data.filter(filter).slice(0, maxItems);
+
+  return enrichProfiles(queryResult);
+}
+
+function createCardsRow(parent, items) {
+  const blogList = document.createElement('ul');
+  blogList.classList.add('related-list');
+
+  items.forEach((value) => {
+    const blogListItem = document.createElement('li');
+    blogListItem.classList.add('related-list-item');
+    const ttr = document.createElement('div');
+    ttr.classList.add('teasertopicrelated');
+    blogListItem.append(ttr);
+    const article = document.createElement('article');
+    article.classList.add('teaser-base');
+
+    article.innerHTML = buildCard(value.path, value.title, value.profiles);
 
     ttr.appendChild(article);
     blogList.appendChild(blogListItem);
   });
 
-  return blogList;
+  parent.appendChild(blogList);
 }
 
-export default async function decorate(block) {
+async function buildAutoRelatedBlogs(block) {
   // get the tag to be fetched
   const tagConf = getConfiguredTag(readBlockConfig(block));
 
   block.innerHTML = ''; // reset
 
+  // create container
   const outerDiv = document.createElement('div');
   outerDiv.classList.add('related-container');
 
   // headline
-  const head4 = document.createElement('h4');
-  const text4head = document.createTextNode(`More ${tagConf}`);
-  head4.appendChild(text4head);
-  outerDiv.appendChild(head4);
+  buildHeadline(outerDiv, tagConf);
 
-  outerDiv.appendChild(await getRelatedArticles(tagConf));
+  // list of cards
+  const relatedArticles = await getRelatedArticles((item) => item.tags.includes(tagConf));
+  createCardsRow(outerDiv, relatedArticles);
+
   block.append(outerDiv);
 
+  // cta section
   const ovr = document.createElement('div');
   ovr.classList.add('btn--light-teal');
   ovr.classList.add('btn--solid');
@@ -104,4 +119,34 @@ export default async function decorate(block) {
     </i>`;
   ovr.appendChild(ovrlink);
   block.append(ovr);
+}
+
+async function buildManualRelatedBlogs(block) {
+  const configuredPaths = [...block.querySelectorAll(':scope > div > div > a')]
+    .map((anchor) => new URL(anchor.href).pathname);
+
+  block.innerHTML = ''; // reset
+
+  // create container
+  const outerDiv = document.createElement('div');
+  outerDiv.classList.add('related-container');
+
+  const relatedArticles = await getRelatedArticles((item) => configuredPaths.includes(item.path));
+  createCardsRow(outerDiv, relatedArticles);
+
+  block.append(outerDiv);
+}
+
+export default async function decorate(block) {
+  // TODO improve variant handling - is this the final logic? Do we want a tag?
+  const variant = document.querySelector('body.blogpost') ? 'auto' : 'manual';
+
+  switch (variant) {
+    case 'auto':
+      await buildAutoRelatedBlogs(block);
+      break;
+    case 'manual':
+    default:
+      await buildManualRelatedBlogs(block);
+  }
 }

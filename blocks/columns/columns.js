35a9bd1410f05598344c6dbd315dd3de27211aeb
Dirk Rudolph <drudolph@adobe.com>
2022-11-25 17:51:08 +0100

    feat: column variations (#89)
    * feat: column variations w/o headlines yet
    * fix: top margin of column title
    * fix: heading colors
    * fix: simplify border in-between, ensure margin between border and top of first chhild
    * fix: align images side by side
    * fix: center images only when said so bei the editor

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,33 @@
+import { loadCSS } from '../../scripts/lib-franklin.js';
+
+function decorateTeasers(cols, block) {
+  loadCSS(`${window.hlx.codeBasePath}/blocks/columns/columns-teasers.css`);
+  const pictures = cols.map((div) => div.querySelector('picture')).filter((picture) => !!picture);
+  if (pictures.length === cols.length) {
+    block.classList.add('columns-teasers-align-images');
+  }
+  pictures.forEach((picture) => picture.closest('p').classList.add('columns-teaser-image'));
+  cols.forEach((div) => {
+    const headings = div.querySelectorAll('h2,h3,h4');
+    let title;
+    if (headings.length > 1) {
+      headings[0].classList.add('columns-column-title');
+      [, title] = headings;
+    } else if (headings.length === 1) {
+      [title] = headings;
+    }
+    if (title) {
+      title.classList.add('columns-teaser-title');
+      if (title.previousElementSibling) title.previousElementSibling.classList.add('columns-teaser-pretitle');
+    }
+  });
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  if (block.matches('.teasers')) {
+    decorateTeasers(cols, block);
+  }
 }

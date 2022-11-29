c65f697d059e49713d87d7dba53ef967ce079007
Hannes Hertach <hanneshertach@gmail.com>
2022-11-29 09:33:30 +0100

    rewrite testimonials code to reduce coupling to blog

--- blocks/testimonial/testimonial.js
+++ blocks/testimonial/testimonial.js
@@ -1,9 +1,41 @@
-import { createCardsList } from '../blog-posts/blog-posts.js';
-import { loadCSS } from '../../scripts/lib-franklin.js';
+const defaultAuthorName = 'Cognizant Netcentric';
+const defaultAuthorRole = '';
+const defaultAuthorImage = '/icons/nc.svg';
+
+function buildCard(card) {
+  const {
+    title, profiles: authorProfile,
+  } = card;
+
+  const cardElement = document.createElement('li');
+  cardElement.classList.add('testimonial-card');
+
+  if (authorProfile.image === '') authorProfile.image = defaultAuthorImage;
+
+  cardElement.innerHTML = `
+    <h2 class="quote">
+      ${title}
+    </h2>
+    <div class="authorprofile-container">
+      <img class="nc-image" src="${authorProfile.image ?? defaultAuthorImage}" itemprop="contentUrl" alt="" sizes="10vw" />
+      <div class="authorprofile-name">${authorProfile.name ?? defaultAuthorName}</div>
+      <div class="authorprofile-position">${authorProfile.role ?? defaultAuthorRole}</div>
+    </div>`;
+
+  return cardElement;
+}
+
+function createCardsList(parent, cards) {
+  const blogList = document.createElement('ul');
+
+  cards.forEach((card) => {
+    blogList.appendChild(buildCard(card));
+  });
+
+  parent.appendChild(blogList);
+}
 
 export default async function decorate(block) {
-  loadCSS('/blocks/blog-posts/blog-card.css');
-
   const cards = [...block.children].map((child) => {
     const profilePicture = child.querySelector('picture > img').getAttribute('src');
     const textContainer = child.children[1];

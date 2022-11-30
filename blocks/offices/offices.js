6905dfc9582643854b100396ff1f462ccf7a3033
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2022-11-30 13:27:55 +0100

    #98 update simplify to 2 block versions

--- blocks/offices/offices.js
+++ blocks/offices/offices.js
@@ -5,9 +5,21 @@ function removeChevronFromContactLinks(textBlock) {
   });
 }
 
+function addTitleClass(textblock) {
+  const btn = '.button-container';
+  const ctaBtn = `${btn} strong`;
+  const hasBtn = textblock.querySelector(btn);
+  const isCallToActionBlock = !hasBtn || (hasBtn && textblock.querySelector(ctaBtn));
+  if (!isCallToActionBlock) {
+    const title = textblock.querySelector('h2');
+    title.classList.add('offices__title');
+  }
+}
+
 export default function decorate(block) {
   const textBlocks = block.querySelectorAll('.offices > div > div:has(h2)');
   textBlocks.forEach((textBlock) => {
     removeChevronFromContactLinks(textBlock);
+    addTitleClass(textBlock);
   });
 }

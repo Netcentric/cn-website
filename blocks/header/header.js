507045bf342960a086a40b749659c02d171c2dce
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-01-10 11:13:22 +0100

    #248 Search is hided in DE

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -196,6 +196,7 @@ export default async function decorate(block) {
       url.search = params;
       window.open(url.href, '_self');
     });
+    if (getLanguagePath() === '/de') navSearch.style.display = 'none';
 
     // mobile language selector
     const langToggleButton = document.createElement('button');

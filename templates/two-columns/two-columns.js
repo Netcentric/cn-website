18e4b1c9a0174056ec2a1e8724ec1bdeb24d1dc0
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-01-20 10:27:31 +0100

    #255 UPDATE 2-col template with dynamic ratio

--- templates/two-columns/two-columns.js
+++ templates/two-columns/two-columns.js
@@ -1,13 +1,50 @@
-import { loadBlock } from '../../scripts/lib-franklin.js';
+import { loadCSS } from '../../scripts/lib-franklin.js';
 
+/**
+ * Checks if the ratio is different from 50-50 and applies the ratio to its column respectively
+ * @param {HTMLElement} columnOne - first column section
+ * @param {HTMLElement} columnTwo - second column section
+ */
+function setRatio(columnOne, columnTwo) {
+  const ratio = document.querySelector('meta[name="column-ratio"]').content;
+  if (ratio) {
+    const isDefault = ratio.includes('50');
+    if (!isDefault) {
+      const [firstRatio, secondRatio] = ratio.split('-');
+      const root = document.querySelector(':root');
+
+      root.style.setProperty('--first-ratio', `${firstRatio}%`);
+      root.style.setProperty('--second-ratio', `${secondRatio}%`);
+
+      columnOne.classList.add('first-column');
+      columnTwo.classList.add('second-column');
+    }
+  }
+}
+
+/**
+ * Modify the page so that it has a two-column template for the first two sections
+ */
 export default function buildAutoBlocks() {
-  const columnsBlock = document.querySelector('.columns:has(.form)');
-  const formBlock = columnsBlock && columnsBlock.querySelector('.form');
+  const main = document.querySelector('main');
+  const columnOne = [...main.children][0];
+  const columnTwo = [...main.children][1];
+  const newSection = document.createElement('section');
 
-  // Setup the element to be loaded as a regular block
-  formBlock.classList.add('block', 'innerblock');
-  formBlock.dataset.blockName = 'form';
-  formBlock.dataset.blockStatus = 'initialized';
+  const observer = new MutationObserver((list) => {
+    list.forEach((change) => {
+      const status = change.target.dataset.sectionStatus;
+      if (status === 'loaded') {
+        loadCSS('/styles/two-columns.css');
+        setRatio(columnOne, columnTwo);
+        main.insertBefore(newSection, columnOne);
+        newSection.classList.add('section-wrapper');
+        newSection.appendChild(columnOne);
+        newSection.appendChild(columnTwo);
+        observer.disconnect();
+      }
+    });
+  });
 
-  loadBlock(formBlock);
+  observer.observe(columnTwo, { attributes: true, attributeFilter: ['data-section-status'] });
 }

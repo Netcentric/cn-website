164e819a53a2e7d76bda114e2a1f3e48641597bc
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-05-17 11:12:44 +0530

    323 Applying solid colrs to the cards

---
+++ blocks/cards-solidcolors/cards-solidcolors.js
@@ -0,0 +1,35 @@
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    li.innerHTML = row.innerHTML;
+    const stylerow = [...block.lastElementChild.children];
+    const divdata = stylerow[1].textContent;
+    const arr = divdata.split(',');
+    [...ul.children].forEach(() => {
+      for (let i = 0; i < arr.length; i += 1) {
+        // eslint-disable-next-line prefer-destructuring
+        ul.children[0].className = arr[0];
+        if (ul.children[1] !== undefined) {
+          // eslint-disable-next-line prefer-destructuring
+          ul.children[1].className = arr[1];
+          if (ul.children[2] !== undefined) {
+          // eslint-disable-next-line prefer-destructuring
+            ul.children[2].className = arr[2];
+          }
+        }
+      }
+    });
+    [...li.children].forEach((div) => {
+      div.className = 'cards-solidcolors-card-body';
+      if (div.innerHTML === 'style') {
+        div.parentNode.className = 'config-hidden';
+        div.parentNode.innerHTML = '';
+      }
+    });
+    ul.append(li);
+  });
+  block.textContent = '';
+  block.append(ul);
+}

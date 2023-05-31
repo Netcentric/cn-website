8b3497d6385f43d2e82b1b9c0540f736a35fea7b
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-05-31 12:41:00 +0530

    283 - Resolving comment with error handling

--- blocks/form/gdpr-confirmation.js
+++ blocks/form/gdpr-confirmation.js
@@ -1,22 +1,20 @@
-// get url parameters
-const curUrl = window.location.href;
-const urlParams = new URLSearchParams(window.location.search);
-const paramdata = urlParams.get('data');
-const paraminit = urlParams.get('init');
-
 function hasQueryParams() {
-  return curUrl.toString().includes('?');
+  return !!window.location.search;
 }
 
 // Send a query parameters to the GDPR API Accept
-export async function acceptresult(accept, urlProd, secondConfig) {
+async function acceptResult(accept, urlProd, sucessRedirect, block) {
+  // get url parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  const paramData = urlParams.get('data');
+  const paramInit = urlParams.get('init');
   const url = urlProd + accept;
   const options = {
     method: 'PUT',
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
     body: JSON.stringify({
-      data: paramdata,
-      init: paraminit,
+      data: paramData,
+      init: paramInit,
       confirm: 'true',
     }),
   };
@@ -24,27 +22,42 @@ export async function acceptresult(accept, urlProd, secondConfig) {
     const response = await fetch(url, options);
     const resData = await response.text();
     if (response.ok) {
-      window.open(secondConfig);
+      window.open(sucessRedirect);
     } else {
       const { message } = JSON.parse(resData);
       throw message;
     }
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(
-      `%cError sending the data: %c${error}`,
-      'color:#f88; background-color:#290002',
-      'color:white; background-color:#290002',
-    );
+    const divError = document.createElement('div');
+    divError.className = 'diverror';
+    divError.innerHTML = 'Something went wrong. Try again';
+    block.appendChild(divError);
+    const span = document.createElement('span');
+    span.innerHTML = 'x';
+    span.className = 'closeerror';
+    divError.appendChild(span);
+    span.addEventListener("click", closeError);
+    function closeError() {
+      divError.style.display = 'none';
+    }
   }
 }
-
+/* eslint-disable import/prefer-default-export */
 export function createConfirmForm(block) {
   const confirmationForm = document.createElement('form');
   const configlIst = [];
+  const configlIstNames = [];
   [...block.children].forEach((row) => {
     configlIst.push(row.children[1].textContent);
+    [row].forEach((div) => {
+      configlIstNames.push(div.children[0].textContent);
+    });
   });
+  const indexBaseurl = configlIstNames.indexOf('API base URL');
+  const indexSuccessRedirect = configlIstNames.indexOf('Success Redirect');
+  const sucessRedirect = configlIst[indexSuccessRedirect];
+  const indexButtonLabel = configlIstNames.indexOf('Button Label');
+  const indexCheckboxText = configlIstNames.indexOf('Checkbox Text');
   block.textContent = '';
   // creates a check box
   const checkbox = document.createElement('input');
@@ -54,25 +67,24 @@ export function createConfirmForm(block) {
   checkbox.className = 'confirmation-checkbox';
   const label = document.createElement('label');
   label.setAttribute('for', 'confirm');
-  const [firstConfig, secondConfig, thirdConfig, fourthConfig] = configlIst;
-  label.innerHTML = fourthConfig;
+  label.innerHTML = configlIst[indexCheckboxText];
   label.className = 'confirm-label';
   // creates a button
   const btn = document.createElement('button');
-  btn.innerText = thirdConfig;
+  btn.innerText = configlIst[indexButtonLabel];
   btn.className = 'button';
   confirmationForm.method = 'POST';
   confirmationForm.append(checkbox, label, btn);
   block.append(confirmationForm);
   // checking query parameters
-  if (!hasQueryParams(curUrl)) {
+  if (!hasQueryParams()) {
     btn.setAttribute('disabled', '');
   }
   // API Variables
   const accept = '/accept';
-  const urlProd = firstConfig;
+  const urlProd = configlIst[indexBaseurl];
   confirmationForm.onsubmit = async (e) => {
     e.preventDefault();
-    acceptresult(accept, urlProd, secondConfig);
+    acceptResult(accept, urlProd, sucessRedirect, block);
   };
 }

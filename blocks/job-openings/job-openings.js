3d4ce7ce4a0addd01969f78f255d2ff6358dde6e
Tomasz Dudek <tomasz.dudek@netcentric.biz>
2023-06-12 13:26:25 +0200

    330 - refactoring according to PR Comments
    - removes not needed environment file
    - improves javascript part

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -1,11 +1,5 @@
-const tagString = '<iframe src="//www.careers-page.com/netcentric" class="embed-job-openings"></iframe>';
-const range = document.createRange();
-const jobOpeningsBlock = document.querySelectorAll('div.job-openings');
-const jobOpeningsBlockItem = jobOpeningsBlock.item(0);
-const emptyDiv = jobOpeningsBlockItem.firstElementChild;
+const iframe = '<iframe src="//www.careers-page.com/netcentric" class="embed-job-openings"></iframe>';
 
-range.selectNode(jobOpeningsBlockItem);
-const documentFragment = range.createContextualFragment(tagString);
-jobOpeningsBlockItem.appendChild(documentFragment);
-
-emptyDiv.remove();
+export default function decorate(block) {
+  block.innerHTML = iframe;
+}

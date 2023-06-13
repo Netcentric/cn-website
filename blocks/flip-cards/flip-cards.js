78956e9e86cfb722a500829ed2d836108ed29c11
Mihai Buricea <mihai.buricea@netcentric.biz>
2023-06-13 12:37:36 +0300

    327 - update linting

--- blocks/flip-cards/flip-cards.js
+++ blocks/flip-cards/flip-cards.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
   const ul = document.createElement('ul');
-  ul.classList.add('flip-cards--wrap');
+  ul.classList.add('flip-cards-wrap');
 
   [...block.children].forEach((row) => {
     const [defaultState, hoverState] = row.children;
 
-    defaultState.classList.add('flip-cards--default');
-    hoverState.classList.add('flip-cards--hover');
+    defaultState.classList.add('flip-cards-default');
+    hoverState.classList.add('flip-cards-hover');
 
     const li = document.createElement('li');
     li.classList.add('flip-cards-item');

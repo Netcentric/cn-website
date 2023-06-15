dd6c4affb43c83a56f3c1a39e59a0d5bdd2e112a
Andreas Haller <andreas.haller@netcentric.biz>
2023-06-15 09:42:34 +0200

    #283: cleanup and streamline GDPR encrypt form block

--- blocks/form/gdpr-encrypt.js
+++ blocks/form/gdpr-encrypt.js
@@ -1,162 +1,88 @@
-const apiClass = 'apiKey';
-const inputApi = document.getElementById(apiClass);
-const main = document.querySelector('main');
-const formBlock = document.querySelector('.gdpr');
-const form = formBlock.querySelector('form');
-const inputs = form.querySelectorAll('input');
-const ticket2Link = '/ticket2link?ticket=';
-const urlProd = 'https://api.netcentric.biz/gdpr';
-const urlBase = urlProd;
+const apiKeyStoreName = 'gdpr-api-key';
+const encryptEndpoint = 'https://api.netcentric.biz/gdpr/ticket2link';
 
-// Fill API key if it is saved in localStorage
-const savedApiKey = localStorage.getItem(apiClass);
-inputApi.value = savedApiKey ?? '';
-
-// Set up Feedback section
-const section = document.createElement('div');
-const SWrapper = document.createElement('div');
-const feedback = document.createElement('div');
-const fbWrapper = document.createElement('div');
-const fbContainer = document.createElement('div');
-section.classList.add('section', 'section-feedback-container');
-SWrapper.classList.add('section-feedback-wrapper');
-feedback.classList.add('feedback');
-fbWrapper.classList.add('feedback-wrapper');
-fbContainer.classList.add('feedback-container');
-main.appendChild(section);
-section.appendChild(SWrapper);
-SWrapper.appendChild(feedback);
-feedback.appendChild(fbWrapper);
-fbWrapper.appendChild(fbContainer);
-
-/**
- * Add an error message to an input field in a form
- * @param {HTMLElement} el HTMLElement
- * @param {string} text text to show as error
- */
-function showErrorMessage(el, text) {
-  const wrapper = el.parentElement;
-  const hasMessage = wrapper.querySelector('.error-message');
-  if (!hasMessage) {
-    const message = document.createElement('p');
-    message.classList.add('error-message');
-    message.innerText = text;
-    wrapper.appendChild(message);
-  }
+function resetResults(resultElements) {
+  resultElements.forEach((p) => {
+    p.classList.remove('show');
+    p.innerHTML = '';
+  });
 }
 
-/**
- * Remove an error message from an input field in a form
- * @param {HTMLElement} el HTMLElement
- */
-function hideErrorMessage(el) {
-  const wrapper = el.parentElement;
-  const message = wrapper.querySelector('.error-message');
-  if (message) wrapper.removeChild(message);
-}
-
-/**
- * Check the inputs value to decide if it needs to show an error message ot not
- * @param {Event} e Event
- */
-function checkErrorMessage(e) {
-  const input = e.target;
-  const errorClass = 'error';
-  if (input.value.trim().length < 1) {
-    input.classList.add(errorClass);
-    showErrorMessage(input, 'This field is required');
-  } else {
-    input.classList.remove(errorClass);
-    hideErrorMessage(input);
-  }
-}
-
-/**
- * Add a feedback message with technical errors and shows it
- * @param {string} message The error message
- */
-function showFeedbackMessage(message) {
-  const title = document.createElement('h4');
-  const text = document.createElement('p');
-  const close = document.createElement('button');
-  title.textContent = 'Something goes wrong';
-  text.innerText = message;
-  close.innerText = 'close message';
-  close.type = 'button';
-  // A close button to hide the message
-  close.onclick = () => {
-    fbContainer.innerHTML = '';
-    feedback.classList.remove('show');
-  };
-  close.classList.add('button');
-  fbContainer.innerHTML = '';
-  fbContainer.append(title, text, close);
-}
-
-/**
- * Open the results in a new tab/window
- * @param {string} data The answered data from the API
- * @param {string} url destination url
- */
-function openInNewWindow(data, url = 'about:blank') {
-  const newWindow = window.open(url, '_blank');
-  newWindow.document.write(data);
-  newWindow.focus();
+function showResult(element, message) {
+  element.classList.add('show');
+  element.innerText = message;
 }
 
 /**
  * Send a query to the GDPR API and save the apiKey to localStorage for further calls
  * @param {{ticket:string, apiKey:string}} result form fields
  */
-async function fetchForm(result) {
-  const { ticket, apiKey } = result;
-  const url = urlBase + ticket2Link + ticket;
+async function encrypt(apiKey, ticket) {
+  localStorage.setItem(apiKeyStoreName, apiKey);
+
+  const endpoint = new URL(encryptEndpoint);
+  endpoint.searchParams.set('ticket', ticket);
   const options = {
     method: 'GET',
     headers: { 'x-api-key': apiKey },
   };
-  try {
-    const response = await fetch(url, options);
-    const data = await response.text();
-    if (data.includes('http')) {
-      if (!savedApiKey || savedApiKey !== apiKey) localStorage.setItem(apiClass, apiKey);
-      openInNewWindow(data, url);
-    } else {
-      const { message } = JSON.parse(data);
-      throw message;
-    }
-  } catch (error) {
-    // Show a message below the form, in the feedback section
-    feedback.classList.add('show');
-    showFeedbackMessage(error);
-
-    // eslint-disable-next-line no-console
-    console.error(
-      `%cError sending the data: %c${error}`,
-      'color:#f88; background-color:#290002',
-      'color:white; background-color:#290002',
-    );
+  const response = await fetch(endpoint, options);
+  if (!response.ok) {
+    throw new Error('Network error.');
   }
+  const data = await response.text();
+  return new URL(data); // exception is thrown if the url is not valid
 }
 
-form.onsubmit = async (e) => {
-  e.preventDefault();
-  const results = {};
-  const inputFields = inputs.length;
-  [...e.target].forEach((el) => {
-    const { id } = el;
-    if (id !== '' && el.value.trim() !== '') {
-      results[id] = el.value;
+export default function decorate(form) {
+  // setup result section
+  const resultElement = document.createElement('div');
+  resultElement.classList.add('form-submission-result');
+  form.append(resultElement);
+  resultElement.innerHTML = `
+  <p class="form-submission-error"></p>
+  <p class="form-submission-success"></p>`;
+  const errorElement = resultElement.querySelector('.form-submission-error');
+  const successElement = resultElement.querySelector('.form-submission-success');
+
+  // get and check input fields
+  const inputApiKey = form.querySelector('input[name="apiKey"]');
+  const inputTicket = form.querySelector('input[name="ticket"]');
+  if (!inputApiKey || !inputTicket) {
+    // eslint-disable-next-line no-console
+    console.error('api key and ticket needed for api call', inputApiKey, inputTicket);
+    showResult(errorElement, 'Invalid form configuration. The two fields "apiKey" and "ticket" are required for link generation.');
+    return;
+  }
+
+  // Fill API key if it is saved in localStorage
+  const savedApiKey = localStorage.getItem(apiKeyStoreName);
+  inputApiKey.value = savedApiKey ?? '';
+
+  // setup form submission
+  const button = form.querySelector('button[name="submit"]');
+  button.addEventListener('click', async (e) => {
+    try {
+      button.disabled = true;
+      resetResults([errorElement, successElement]);
+      if (form.checkValidity()) {
+        e.preventDefault();
+        const link = await encrypt(inputApiKey.value.trim(), inputTicket.value.trim());
+        showResult(successElement, '');
+        successElement.innerHTML = `Right click to copy link: <a href="${link.toString}">Confirmation link</a>`;
+      }
+    } catch (error) {
+      showResult(errorElement, error);
+
+      // eslint-disable-next-line no-console
+      console.error(
+        `%cError encypting: %c${error}`,
+        'color:#f88; background-color:#290002',
+        'color:white; background-color:#290002',
+        error,
+      );
+    } finally {
+      button.disabled = false;
     }
   });
-
-  const { length } = Object.entries(results);
-  // fetch the data
-  if (length === inputFields) await fetchForm(results);
-};
-
-form.onkeyup = (e) => checkErrorMessage(e);
-
-inputs.forEach((input) => {
-  input.onblur = (e) => checkErrorMessage(e);
-});
+}

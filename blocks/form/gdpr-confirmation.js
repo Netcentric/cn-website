2df84c49f327a0aec05a8f70e9e0f69140abe5fe
Andreas Haller <andreas.haller@netcentric.biz>
2023-06-19 10:18:45 +0200

    #283:  - stay on same tab for GDPR accept success redirect  - remove previous errors when submitting a second time  - avoid double submit by disabling button on submit

--- blocks/form/gdpr-confirmation.js
+++ blocks/form/gdpr-confirmation.js
@@ -5,31 +5,33 @@ function hasQueryParams() {
 }
 
 // Send a query parameters to the GDPR API Accept
-async function acceptResult(accept, urlProd, sucessRedirect, block) {
-  // get url parameters
-  const urlParams = new URLSearchParams(window.location.search);
-  const paramData = urlParams.get('data');
-  const paramInit = urlParams.get('init');
-  const url = urlProd + accept;
-  const options = {
-    method: 'PUT',
-    headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    body: JSON.stringify({
-      data: paramData,
-      init: paramInit,
-      confirm: 'true',
-    }),
-  };
+async function acceptResult(accept, urlProd, sucessRedirect, block, submitButton) {
   try {
+    submitButton.disabled = true;
+    // get url parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const paramData = urlParams.get('data');
+    const paramInit = urlParams.get('init');
+    const url = urlProd + accept;
+    const options = {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({
+        data: paramData,
+        init: paramInit,
+        confirm: 'true',
+      }),
+    };
     const response = await fetch(url, options);
     const resData = await response.text();
     if (response.ok) {
-      window.open(sucessRedirect);
+      window.location.href = sucessRedirect;
     } else {
       const { message } = JSON.parse(resData);
       throw message;
     }
   } catch (error) {
+    [...block.querySelectorAll('.error')].forEach((div) => div.remove());
     const divError = document.createElement('div');
     divError.className = 'error';
     divError.innerHTML = 'Something went wrong. Please try again.';
@@ -48,6 +50,8 @@ async function acceptResult(accept, urlProd, sucessRedirect, block) {
       'color:#f88; background-color:#290002',
       'color:white; background-color:#290002',
     );
+  } finally {
+    submitButton.disabled = false;
   }
 }
 
@@ -75,13 +79,13 @@ export default function decorate(block) {
   block.append(confirmationForm);
   // checking query parameters
   if (!hasQueryParams()) {
-    btn.setAttribute('disabled', '');
+    btn.disabled = true;
   }
   // API Variables
   const accept = '/accept';
   const urlProd = config['api-base-url'];
   confirmationForm.onsubmit = async (e) => {
     e.preventDefault();
-    acceptResult(accept, urlProd, sucessRedirect, block);
+    acceptResult(accept, urlProd, sucessRedirect, block, btn);
   };
 }

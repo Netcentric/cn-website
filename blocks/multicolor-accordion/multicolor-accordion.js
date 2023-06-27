651465a668f096105b7325004bd8fc872cc55a01
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-06-27 16:39:22 +0530

    329- Divide decorate function into multiple functions

--- blocks/multicolor-accordion/multicolor-accordion.js
+++ blocks/multicolor-accordion/multicolor-accordion.js
@@ -3,25 +3,45 @@ function setLabel(label, btn, btnText) {
   if (label === 'Button Label') {
     btn.innerText = btnText;
   } else {
-    btn.innerText = 'Details';
+    btn.innerText = `${window.placeholders?.default?.Details || 'Details'}`;
     if (checkClass) {
-      btn.innerText = 'Close';
+      btn.innerText = `${window.placeholders?.default?.Close || 'Close'}`;
     }
   }
 }
 
-export default async function decorate(block) {
-  const accordionItems = Array.from(block.children);
-  const buttonLabelRow = Array.from(block.lastElementChild.children);
-  const btnLabel = buttonLabelRow[0].textContent;
-  const btnLabelText = buttonLabelRow[1].textContent;
-  const btnLabelTextArr = btnLabelText.split(',');
-  buttonLabelRow[0].classList.add('config-hidden');
+let accordionItemOpen = null;
+const openAttribute = 'aria-expanded';
+const hiddenAttribute = 'aria-hidden';
 
-  let accordionItemOpen = null;
+function toggleAccordian(buttonWrapper, btnLabel, btnLabelTextArr) {
+  buttonWrapper.addEventListener('click', () => {
+    if (accordionItemOpen) {
+      const panelClosed = accordionItemOpen.nextElementSibling;
+      panelClosed.style.maxHeight = null;
+      accordionItemOpen.setAttribute(openAttribute, false);
+      accordionItemOpen.parentElement.setAttribute(hiddenAttribute, true);
+      const btn = accordionItemOpen.querySelector('.btn');
+      btn.className = 'btn details-button';
+      setLabel(btnLabel, btn, btnLabelTextArr[0]);
+    }
+    if (accordionItemOpen === buttonWrapper) {
+      accordionItemOpen = null;
+      return;
+    }
+    accordionItemOpen = buttonWrapper;
+    buttonWrapper.setAttribute(openAttribute, true);
+    buttonWrapper.parentElement.setAttribute(hiddenAttribute, false);
+    const panel = buttonWrapper.nextElementSibling;
+    panel.style.maxHeight = `${panel.scrollHeight}px`;
+    const closeBtn = accordionItemOpen.children[1];
+    closeBtn.className = 'btn close-button';
+    setLabel(btnLabel, closeBtn, btnLabelTextArr[1]);
+  });
+}
+
+function getAccordianItems(accordionItems, btnLabel, btnLabelTextArr) {
   accordionItems.forEach((accordionItem, index) => {
-    const openAttribute = 'aria-expanded';
-    const hiddenAttribute = 'aria-hidden';
     const nodes = Array.from(accordionItem.children);
     const buttonWrapper = document.createElement('button');
     buttonWrapper.append(nodes[0]);
@@ -40,30 +60,20 @@ export default async function decorate(block) {
     newItem.id = `panel${index + 1}`;
     accordionItem.replaceWith(newItem);
 
-    buttonWrapper.addEventListener('click', () => {
-      if (accordionItemOpen) {
-        const panelClosed = accordionItemOpen.nextElementSibling;
-        panelClosed.style.maxHeight = null;
-        accordionItemOpen.setAttribute(openAttribute, false);
-        accordionItemOpen.parentElement.setAttribute(hiddenAttribute, true);
-        const btn = accordionItemOpen.querySelector('.btn');
-        btn.className = 'btn details-button';
-        setLabel(btnLabel, btn, btnLabelTextArr[0]);
-      }
-      if (accordionItemOpen === buttonWrapper) {
-        accordionItemOpen = null;
-        return;
-      }
-      accordionItemOpen = buttonWrapper;
-      buttonWrapper.setAttribute(openAttribute, true);
-      buttonWrapper.parentElement.setAttribute(hiddenAttribute, false);
-      const panel = buttonWrapper.nextElementSibling;
-      panel.style.maxHeight = `${panel.scrollHeight}px`;
-      const closeBtn = accordionItemOpen.children[1];
-      closeBtn.className = 'btn close-button';
-      setLabel(btnLabel, closeBtn, btnLabelTextArr[1]);
-    });
+    toggleAccordian(buttonWrapper, btnLabel, btnLabelTextArr);
   });
+}
+
+export default async function decorate(block) {
+  const accordionItems = Array.from(block.children);
+  const buttonLabelRow = Array.from(block.lastElementChild.children);
+  const btnLabel = buttonLabelRow[0].textContent;
+  const btnLabelText = buttonLabelRow[1].textContent;
+  const btnLabelTextArr = btnLabelText.split(',');
+  buttonLabelRow[0].classList.add('config-hidden');
+
+  getAccordianItems(accordionItems, btnLabel, btnLabelTextArr);
+
   const configHidden = block.querySelector('.config-hidden');
   if (configHidden) {
     configHidden.parentElement.remove();

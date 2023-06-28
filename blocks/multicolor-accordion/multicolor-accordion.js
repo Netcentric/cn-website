6d1051a3e066f6ccef50cce066285d551fa7974a
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-06-28 14:26:44 +0530

    329 - Removing button row

--- blocks/multicolor-accordion/multicolor-accordion.js
+++ blocks/multicolor-accordion/multicolor-accordion.js
@@ -1,20 +1,21 @@
-function setLabel(label, btn, btnText) {
-  const checkClass = btn.classList.contains('close-button');
-  if (label === 'Button Label') {
-    btn.innerText = btnText;
-  } else {
-    btn.innerText = `${window.placeholders?.default?.Details || 'Details'}`;
-    if (checkClass) {
-      btn.innerText = `${window.placeholders?.default?.Close || 'Close'}`;
-    }
-  }
-}
-
 let accordionItemOpen = null;
 const openAttribute = 'aria-expanded';
 const hiddenAttribute = 'aria-hidden';
+const btnOpen = `${window.placeholders?.default?.Details || 'Details'}`;
+const closenewBtn = `${window.placeholders?.default?.Close || 'Close'}`;
 
-function toggleAccordian(buttonWrapper, btnLabel, btnLabelTextArr) {
+function getOpenAccordian(buttonWrapper) {
+  accordionItemOpen = buttonWrapper;
+  buttonWrapper.setAttribute(openAttribute, true);
+  buttonWrapper.parentElement.setAttribute(hiddenAttribute, false);
+  const panel = buttonWrapper.nextElementSibling;
+  panel.style.maxHeight = `${panel.scrollHeight}px`;
+  const closeBtn = accordionItemOpen.children[1];
+  closeBtn.className = 'btn close-button';
+  closeBtn.innerText = closenewBtn;
+}
+
+function toggleAccordian(buttonWrapper) {
   buttonWrapper.addEventListener('click', () => {
     if (accordionItemOpen) {
       const panelClosed = accordionItemOpen.nextElementSibling;
@@ -23,32 +24,23 @@ function toggleAccordian(buttonWrapper, btnLabel, btnLabelTextArr) {
       accordionItemOpen.parentElement.setAttribute(hiddenAttribute, true);
       const btn = accordionItemOpen.querySelector('.btn');
       btn.className = 'btn details-button';
-      setLabel(btnLabel, btn, btnLabelTextArr[0]);
+      btn.innerText = btnOpen;
     }
     if (accordionItemOpen === buttonWrapper) {
       accordionItemOpen = null;
       return;
     }
-    accordionItemOpen = buttonWrapper;
-    buttonWrapper.setAttribute(openAttribute, true);
-    buttonWrapper.parentElement.setAttribute(hiddenAttribute, false);
-    const panel = buttonWrapper.nextElementSibling;
-    panel.style.maxHeight = `${panel.scrollHeight}px`;
-    const closeBtn = accordionItemOpen.children[1];
-    closeBtn.className = 'btn close-button';
-    setLabel(btnLabel, closeBtn, btnLabelTextArr[1]);
+    getOpenAccordian(buttonWrapper);
   });
 }
 
-function getAccordianItems(accordionItems, btnLabel, btnLabelTextArr) {
+function getAccordianItems(accordionItems) {
   accordionItems.forEach((accordionItem, index) => {
     const nodes = Array.from(accordionItem.children);
     const buttonWrapper = document.createElement('button');
-    buttonWrapper.append(nodes[0]);
     const span = document.createElement('span');
-    span.append(btnLabelTextArr[0]);
     span.className = 'btn details-button';
-    setLabel(btnLabel, span, btnLabelTextArr[0]);
+    span.innerText = btnOpen;
     buttonWrapper.append(nodes[0], span);
     buttonWrapper.classList.add('accordion-trigger');
     buttonWrapper.setAttribute(openAttribute, false, 'aria-controls', `panel${index + 1}`);
@@ -60,22 +52,11 @@ function getAccordianItems(accordionItems, btnLabel, btnLabelTextArr) {
     newItem.id = `panel${index + 1}`;
     accordionItem.replaceWith(newItem);
 
-    toggleAccordian(buttonWrapper, btnLabel, btnLabelTextArr);
+    toggleAccordian(buttonWrapper);
   });
 }
 
 export default async function decorate(block) {
   const accordionItems = Array.from(block.children);
-  const buttonLabelRow = Array.from(block.lastElementChild.children);
-  const btnLabel = buttonLabelRow[0].textContent;
-  const btnLabelText = buttonLabelRow[1].textContent;
-  const btnLabelTextArr = btnLabelText.split(',');
-  buttonLabelRow[0].classList.add('config-hidden');
-
-  getAccordianItems(accordionItems, btnLabel, btnLabelTextArr);
-
-  const configHidden = block.querySelector('.config-hidden');
-  if (configHidden) {
-    configHidden.parentElement.remove();
-  }
+  getAccordianItems(accordionItems);
 }

d235d74fc41d83a56fb49ed44291c0f0fe018398
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-06-30 17:03:23 +0530

    329 - changes to remove bugs

--- blocks/multicolor-accordion/multicolor-accordion.js
+++ blocks/multicolor-accordion/multicolor-accordion.js
@@ -1,58 +1,58 @@
 let accordionItemOpen = null;
 const openAttribute = 'aria-expanded';
 const hiddenAttribute = 'aria-hidden';
-const btnOpen = `${window.placeholders?.default?.Details || 'Details'}`;
-const closenewBtn = `${window.placeholders?.default?.Close || 'Close'}`;
+const btnTextOpen = `${window.placeholders?.default?.Details || 'Details'}`;
+const btnTextClose = `${window.placeholders?.default?.Close || 'Close'}`;
 
-function getOpenAccordian(buttonWrapper) {
+function getOpenAccordian(buttonWrapper, content) {
   accordionItemOpen = buttonWrapper;
   buttonWrapper.setAttribute(openAttribute, true);
-  buttonWrapper.parentElement.setAttribute(hiddenAttribute, false);
+  content.setAttribute(hiddenAttribute, false);
   const panel = buttonWrapper.nextElementSibling;
   panel.style.maxHeight = `${panel.scrollHeight}px`;
-  const closeBtn = accordionItemOpen.children[1];
-  closeBtn.className = 'btn close-button';
-  closeBtn.innerText = closenewBtn;
+  const openAccordianBtn = accordionItemOpen.children[1];
+  openAccordianBtn.className = 'btn close-button';
+  openAccordianBtn.innerText = btnTextClose;
 }
 
-function toggleAccordian(buttonWrapper) {
+function toggleAccordian(buttonWrapper, content) {
   buttonWrapper.addEventListener('click', () => {
     if (accordionItemOpen) {
       const panelClosed = accordionItemOpen.nextElementSibling;
       panelClosed.style.maxHeight = null;
       accordionItemOpen.setAttribute(openAttribute, false);
-      accordionItemOpen.parentElement.setAttribute(hiddenAttribute, true);
+      content.setAttribute(hiddenAttribute, true);
       const btn = accordionItemOpen.querySelector('.btn');
       btn.className = 'btn details-button';
-      btn.innerText = btnOpen;
+      btn.innerText = btnTextOpen;
     }
     if (accordionItemOpen === buttonWrapper) {
       accordionItemOpen = null;
       return;
     }
-    getOpenAccordian(buttonWrapper);
+    getOpenAccordian(buttonWrapper, content);
   });
 }
 
 function getAccordianItems(accordionItems) {
   accordionItems.forEach((accordionItem, index) => {
-    const nodes = Array.from(accordionItem.children);
+    const [title, content] = accordionItem.children;
     const buttonWrapper = document.createElement('button');
     const span = document.createElement('span');
     span.className = 'btn details-button';
-    span.innerText = btnOpen;
-    buttonWrapper.append(nodes[0], span);
+    span.innerText = btnTextOpen;
+    buttonWrapper.append(title, span);
     buttonWrapper.classList.add('accordion-trigger');
-    buttonWrapper.setAttribute(openAttribute, false, 'aria-controls', `panel${index + 1}`);
-    nodes[1].className = 'accordion-content';
+    buttonWrapper.setAttribute(openAttribute, false);
+    buttonWrapper.setAttribute('aria-controls', `panel${index + 1}`);
+    content.className = 'accordion-content';
+    content.setAttribute(hiddenAttribute, true);
     const newItem = document.createElement('div');
-    newItem.append(buttonWrapper, nodes[1]);
+    newItem.append(buttonWrapper, content);
     newItem.classList.add('accordion-item');
-    newItem.setAttribute(hiddenAttribute, true);
-    newItem.id = `panel${index + 1}`;
+    content.id = `panel${index + 1}`;
     accordionItem.replaceWith(newItem);
-
-    toggleAccordian(buttonWrapper);
+    buttonWrapper.addEventListener('click', toggleAccordian(buttonWrapper, content));
   });
 }
 

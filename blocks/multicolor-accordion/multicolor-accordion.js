1ffbe3547a1c16aa732b4610fce0e4581d9fcabb
sonal.dhekale <sonal.dhekale@netcentric.com>
2023-07-03 19:26:16 +0530

    329 - bugs fix with ul li

--- blocks/multicolor-accordion/multicolor-accordion.js
+++ blocks/multicolor-accordion/multicolor-accordion.js
@@ -16,48 +16,45 @@ function getOpenAccordian(buttonWrapper, content) {
 }
 
 function toggleAccordian(buttonWrapper, content) {
-  buttonWrapper.addEventListener('click', () => {
-    if (accordionItemOpen) {
-      const panelClosed = accordionItemOpen.nextElementSibling;
-      panelClosed.style.maxHeight = null;
-      accordionItemOpen.setAttribute(openAttribute, false);
-      content.setAttribute(hiddenAttribute, true);
-      const btn = accordionItemOpen.querySelector('.btn');
-      btn.className = 'btn details-button';
-      btn.innerText = btnTextOpen;
-    }
-    if (accordionItemOpen === buttonWrapper) {
-      accordionItemOpen = null;
-      return;
-    }
-    getOpenAccordian(buttonWrapper, content);
-  });
+  if (accordionItemOpen) {
+    const panelClosed = accordionItemOpen.nextElementSibling;
+    panelClosed.style.maxHeight = null;
+    accordionItemOpen.setAttribute(openAttribute, false);
+    content.setAttribute(hiddenAttribute, true);
+    const btn = accordionItemOpen.querySelector('.btn');
+    btn.className = 'btn details-button';
+    btn.innerText = btnTextOpen;
+  }
+  if (accordionItemOpen === buttonWrapper) {
+    accordionItemOpen = null;
+    return;
+  }
+  getOpenAccordian(buttonWrapper, content);
 }
 
-function getAccordianItems(accordionItems) {
-  accordionItems.forEach((accordionItem, index) => {
-    const [title, content] = accordionItem.children;
-    const buttonWrapper = document.createElement('button');
-    const span = document.createElement('span');
-    span.className = 'btn details-button';
-    span.innerText = btnTextOpen;
-    buttonWrapper.append(title, span);
-    buttonWrapper.classList.add('accordion-trigger');
-    buttonWrapper.setAttribute(openAttribute, false);
-    buttonWrapper.setAttribute('aria-controls', `panel${index + 1}`);
-    content.className = 'accordion-content';
-    content.setAttribute(hiddenAttribute, true);
-    const newItem = document.createElement('div');
-    newItem.append(buttonWrapper, content);
-    newItem.classList.add('accordion-item');
-    content.id = `panel${index + 1}`;
-    accordionItem.replaceWith(newItem);
+function getAccordianItems(accordionItem, index) {
+  const ul = document.createElement('ul');
+  const [title, content] = accordionItem.children;
+  const buttonWrapper = document.createElement('button');
+  const span = document.createElement('span');
+  span.className = 'btn details-button';
+  span.innerText = btnTextOpen;
+  buttonWrapper.append(title, span);
+  buttonWrapper.classList.add('accordion-trigger');
+  buttonWrapper.setAttribute(openAttribute, false);
+  buttonWrapper.setAttribute('aria-controls', `panel${index + 1}`);
+  content.className = 'accordion-content';
+  content.setAttribute(hiddenAttribute, true);
+  const newItem = document.createElement('li');
+  newItem.append(buttonWrapper, content);
+  newItem.classList.add('accordion-item');
+  content.id = `panel${index + 1}`;
+  ul.append(newItem);
+  accordionItem.replaceWith(ul);
 
-    buttonWrapper.addEventListener('click', toggleAccordian(buttonWrapper, content));
-  });
+  buttonWrapper.addEventListener('click', () => toggleAccordian(buttonWrapper, content));
 }
 
 export default async function decorate(block) {
-  const accordionItems = Array.from(block.children);
-  getAccordianItems(accordionItems);
+  [...block.children].forEach(getAccordianItems);
 }

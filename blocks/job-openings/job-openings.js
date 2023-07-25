83acd57380c798dfc89b58f2051aa0e11187d9af
Tomasz Dudek <tomasz.dudek@netcentric.biz>
2023-07-25 15:36:14 +0200

    update commit
    - adapts to Manatal solution proposal
    - adds functionality to read remote content height.

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -1,5 +1,18 @@
-const iframe = '<iframe src="//www.careers-page.com/netcentric" class="embed-job-openings"></iframe>';
+const iframe = '<iframe src="//www.careers-page.com/netcentric" class="embed-job-openings" style="width:100%; height:3150px; border:none;" scrolling="no"></iframe>';
+
+function resizeIframeToFitContent(target) {
+  try {
+    target.style.height = `${target.contentWindow.document.body.scrollHeight}px`;
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
+}
 
 export default function decorate(block) {
   block.innerHTML = iframe;
+
+  const iframeElement = block.querySelector('iframe');
+
+  iframeElement.addEventListener('load', (event) => {
+    resizeIframeToFitContent(event.target);
+  });
 }

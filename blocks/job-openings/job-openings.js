5e3b5835cdee3bdf00b3138a4d337f9059f2cb8e
Victor-AcostaCNC <victor.acosta@netcentric.biz>
2023-10-06 17:23:57 +0200

    Fix #391-3d attempt to improve lighthouse scores

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -2,64 +2,47 @@ const iframeHTML = `
   <iframe src="https://www.careers-page.com/netcentric" 
           class="embed-job-openings" 
           style="width:100%; border:none;" 
-          scrolling="no"
-          loading="lazy">
+          scrolling="no">
   </iframe>
 `;
 
-function resizeIframeToFitContent(target) {
-  try {
-    requestAnimationFrame(() => {
-      target.style.height = `${target.contentWindow.document.body.scrollHeight}px`;
-    });
-  } catch (error) {
-    console.error('Could not resize iframe:', error);
-  }
-}
+let resizeTimer;
 
 function setIframeHeightBasedOnViewport(iframeElement) {
   const width = window.innerWidth;
-  let newHeight = '2300px';
-
   if (width <= 768) {
-    newHeight = '5000px';
+    iframeElement.style.height = '5000px';
   } else if (width <= 1024) {
-    newHeight = '4200px';
-  }
-
-  if (iframeElement.style.height !== newHeight) {
-    requestAnimationFrame(() => {
-      iframeElement.style.height = newHeight;
-    });
+    iframeElement.style.height = '4200px';
+  } else {
+    iframeElement.style.height = '2300px';
   }
 }
 
-function init(block, iframeElement) {
-  setIframeHeightBasedOnViewport(iframeElement);
+function decorate(block) {
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        block.innerHTML = iframeHTML;
+        const iframeElement = block.querySelector('iframe');
+        setIframeHeightBasedOnViewport(iframeElement);
+        observer.disconnect();
+      }
+    });
+    observer.observe(block);
+  } else {
+    block.innerHTML = iframeHTML;
+    const iframeElement = block.querySelector('iframe');
+    setIframeHeightBasedOnViewport(iframeElement);
+  }
 
-  iframeElement.addEventListener('load', (event) => {
-    resizeIframeToFitContent(event.target);
-  });
-
-  let resizeTimer;
   window.addEventListener('resize', () => {
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(() => {
+      const iframeElement = block.querySelector('iframe');
       setIframeHeightBasedOnViewport(iframeElement);
     }, 200);
   });
 }
 
-export default function decorate(block) {
-  block.innerHTML = iframeHTML;
-  const iframeElement = block.querySelector('iframe');
-
-  const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      init(block, iframeElement);
-      observer.disconnect(); // stop observing once initialized
-    }
-  });
-
-  observer.observe(block);
-}
+export default decorate;

646b0aefa14176d4040315868f31f99ee3ca1559
Victor-AcostaCNC <victor.acosta@netcentric.biz>
2023-10-06 15:24:02 +0200

    Fix #391-Add size queries to iframe

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -1,4 +1,4 @@
-const iframe = '<iframe src="https://www.careers-page.com/netcentric" class="embed-job-openings" style="width:100%; height:2500px; border:none;" scrolling="no"></iframe>';
+const iframeHTML = '<iframe src="https://www.careers-page.com/netcentric" class="embed-job-openings" style="width:100%; border:none;" scrolling="no"></iframe>';
 
 function resizeIframeToFitContent(target) {
   try {
@@ -7,12 +7,29 @@ function resizeIframeToFitContent(target) {
   } catch (error) {}
 }
 
+function setIframeHeightBasedOnViewport(iframeElement) {
+  const width = window.innerWidth;
+  if (width <= 768) { // Mobile
+    iframeElement.style.height = '5000px';
+  } else if (width <= 1024) { // Tablet
+    iframeElement.style.height = '4200px';
+  } else { // Desktop
+    iframeElement.style.height = '2300px';
+  }
+}
+
 export default function decorate(block) {
-  block.innerHTML = iframe;
+  block.innerHTML = iframeHTML;
 
   const iframeElement = block.querySelector('iframe');
 
+  setIframeHeightBasedOnViewport(iframeElement);
+
   iframeElement.addEventListener('load', (event) => {
     resizeIframeToFitContent(event.target);
+  });
+
+  window.addEventListener('resize', () => {
+    setIframeHeightBasedOnViewport(iframeElement);
   });
 }

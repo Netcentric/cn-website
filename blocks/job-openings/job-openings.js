112ead8a6f542ab2f385ae6811742544d9f41328
Victor-AcostaCNC <victor.acosta@netcentric.biz>
2023-10-09 16:12:17 +0200

    Fix #391-iframe refactor

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -1,18 +1,22 @@
-const iframe = '<iframe src="https://www.careers-page.com/netcentric" class="embed-job-openings" style="width:100%; border:none;" scrolling="no"></iframe>';
+const iframeElement = document.createElement('iframe');
+iframeElement.src = 'https://www.careers-page.com/netcentric';
+iframeElement.className = 'embed-job-openings';
 
 function resizeIframeToFitContent(target) {
   try {
     target.style.height = `${target.contentWindow.document.body.scrollHeight}px`;
-    // eslint-disable-next-line no-empty
-  } catch (error) {}
+  } catch (error) {
+    console.error('Could not resize iframe:', error);
+  }
 }
 
 export default function decorate(block) {
-  block.innerHTML = iframe;
-
-  const iframeElement = block.querySelector('iframe');
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(iframeElement);
 
   iframeElement.addEventListener('load', (event) => {
     resizeIframeToFitContent(event.target);
   });
+
+  block.appendChild(fragment);
 }

bdc22ad32074b9ada5d6a18196598dea6aac97b4
Victor-AcostaCNC <victor.acosta@netcentric.biz>
2023-10-09 15:51:06 +0200

    Fix #391-Set the document height on the css instead of the iFrame

--- blocks/job-openings/job-openings.js
+++ blocks/job-openings/job-openings.js
@@ -1,72 +1,18 @@
-const iframeSrc = 'https://www.careers-page.com/netcentric';
-let iframeElement = null;
-let resizing = false;
+const iframe = '<iframe src="https://www.careers-page.com/netcentric" class="embed-job-openings" style="width:100%; border:none;" scrolling="no"></iframe>';
 
-function resizeIframeToFitContent() {
-  if (iframeElement) {
-    iframeElement.style.height = `${iframeElement.contentWindow.document.body.scrollHeight}px`;
-  }
-}
-
-function setIframeHeightBasedOnViewport() {
-  const width = window.innerWidth;
-  let newHeight = '2300px';
-
-  if (width <= 768) {
-    newHeight = '5000px';
-  } else if (width <= 1024) {
-    newHeight = '4200px';
-  }
-
-  if (iframeElement && iframeElement.style.height !== newHeight) {
-    iframeElement.style.height = newHeight;
-  }
-}
-
-const resizeObserver = new ResizeObserver(() => {
-  setIframeHeightBasedOnViewport();
-});
-
-function initIframe(block) {
-  const iframe = document.createElement('iframe');
-  iframe.src = iframeSrc;
-  iframe.classList.add('embed-job-openings');
-  iframe.style.cssText = 'width:100%; height:0; border:none;';
-  iframe.scrolling = 'no';
-  iframe.loading = 'lazy';
-  block.appendChild(iframe);
-  iframeElement = iframe;
-
-  iframeElement.addEventListener('load', () => {
-    resizeIframeToFitContent();
-  });
-
-  setIframeHeightBasedOnViewport();
+function resizeIframeToFitContent(target) {
+  try {
+    target.style.height = `${target.contentWindow.document.body.scrollHeight}px`;
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
 }
 
 export default function decorate(block) {
-  const observerOptions = {
-    rootMargin: '0px',
-    threshold: 0.2,
-  };
+  block.innerHTML = iframe;
 
-  const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      initIframe(block);
-      observer.disconnect();
-    }
-  }, observerOptions);
-  observer.observe(block);
+  const iframeElement = block.querySelector('iframe');
 
-  resizeObserver.observe(iframeElement);
-
-  window.addEventListener('resize', () => {
-    if (!resizing) {
-      resizing = true;
-      requestAnimationFrame(() => {
-        setIframeHeightBasedOnViewport();
-        resizing = false;
-      });
-    }
+  iframeElement.addEventListener('load', (event) => {
+    resizeIframeToFitContent(event.target);
   });
 }

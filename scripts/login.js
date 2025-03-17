d69dbecc82cde541fee170a8e84821c98023d644
Joan Beltran <joan.beltran@netcentric.biz>
2025-03-17 16:36:28 +0100

    sign in form

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-import { handleHardReload, getImageURL } from './personalisation-helpers.js';
+import { handleHardReload } from './personalisation-helpers.js';
 
 let userLoggedIn = false;
 
@@ -82,6 +82,152 @@ function createDialog() {
   return dialog;
 }
 
+function generateUUID() {
+  let d = new Date().getTime();
+
+  // Use high-precision timer if available
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now();
+  }
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+async function post(request, callback) {
+  try {
+    const response = await fetch(request);
+    const result = await response.json();
+    console.log("Success:", result);
+    if (callback) callback();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function  sentSignInData(form  , callback) {
+  const data = {
+    "xdmMeta": {
+      "schemaRef": {
+        "id": "https://ns.adobe.com/netcentricgmbh/schemas/709781f368368fceb6e2fb8c62b4def7ad2d6c2e91f98551",
+        "contentType": "application/vnd.adobe.xed-full+json;version=1"
+      }
+    },
+    "xdmEntity": {
+      "_id": generateUUID(),
+      "identityMap": {
+        "email": [
+          {
+            "id": form.email.value
+          }
+        ]
+      },
+      "consents": {
+        "marketing": {
+          "preferred": "email",
+          "postalMail": {
+            "val": "y"
+          },
+          "email": {
+            "val": "y"
+          },
+          "call": {
+            "val": "n"
+          },
+          "any": {
+            "val": "y"
+          }
+        }
+      },
+      "person": {
+        "name": {
+          "lastName": form.lastName.value,
+          "firstName": form.firstName.value
+        }
+      },
+      "homeAddress": {
+        "label": form.company.value
+      },
+      "personalEmail": {
+        "address": form.email.value
+      }
+    }
+  };
+
+  const request = new Request("https://dcs.adobedc.net/collection/d51a67adf7de6444b4bd4be318417667b13aea942d5d1edd548ad7174419a7be?syncValidation=true", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  post(request, callback);
+}
+
+function createSignInButton(button) {
+  if (userLoggedIn) return;
+  const signInButton = document.createElement('a');
+  signInButton.href = '#';
+  signInButton.textContent = 'Sign In';
+  button.parentElement.append(signInButton);
+
+  const dialog = document.createElement('dialog');
+  dialog.className = 'login-dialog';
+  dialog.innerHTML = `
+    <form name="signInForm">
+      <div class="login-dialog-content">
+        <div class="form-wrapper">
+          <label for="firstName">First Name</label>
+          <input type="text" name="firstName" required>
+          <label for="lastName">Last Name</label>
+          <input type="text" name="lastName" required>
+          <label for="company">Company</label>
+          <input type="text" name="company" required>
+          <label for="email">Email</label>
+          <input type="text" name="email" data-validate="email" required>
+          <label for="password">Password</label>
+          <input type="password" name="password" required>
+          <button type="submit">Sign In</button>   
+        </div>
+      </div>  
+    </form>
+  `;
+  window.document.body.appendChild(dialog);
+  const submitButton = dialog.querySelector('button');
+
+  signInButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    dialog.showModal();
+  });
+
+  dialog.addEventListener('click', (e) => {
+    if (e.target === dialog) {
+      dialog.close();
+      resetValidationErrors(document.forms.signInForm);
+    }
+  });
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const form = document.forms.signInForm;
+    const isValid = validateForm(form);
+
+
+    if (isValid) {
+      sentSignInData(form, () => {
+        dialog.close();
+        document.cookie = `ncUser=${form.email.value}`;
+        userLoggedIn = true;
+        handleHardReload(window.location.href);
+      });
+    }
+  });
+}
+
 function setUpButtonText(button) {
   const firstName = window.personalizationData.content?.firstName ? `${window.personalizationData.content.firstName},` : '';
   button.textContent = userLoggedIn ? `${firstName} Log Out` : 'Log In';
@@ -101,11 +247,11 @@ function removeUserCookie() {
 export default function initLogIn(button) {
   const dialog = createDialog();
   const submitButton = dialog.querySelector('button');
-  const logo = document.querySelector('.icon-netcentric-logo');
   window.personalizationData = window.personalizationData || {};
   userLoggedIn = getCookie('ncUser');
   setUpButtonText(button);
   button.href = '#';
+  createSignInButton(button)
 
   button.addEventListener('click', (e) => {
     e.preventDefault();
